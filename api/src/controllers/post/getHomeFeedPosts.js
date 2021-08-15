013b3cc9fcb284f9a2344abf02e678eb09e23998
Joohyung Lee <something1712@gmail.com>
2021-08-15 16:54:51 -0500

    made changes to how home feed posts are fetched

--- api/src/controllers/post/getHomeFeedPosts.js
+++ api/src/controllers/post/getHomeFeedPosts.js
@@ -1,64 +1,122 @@
 const pool = require("../../pool");
 
 const PostRepo = require("../../repos/post-repo");
+const UserRepo = require("../../repos/user-repo");
+
+const calculatePaginationIndexes = require("../../utils/common/calculatePaginationIndexes");
 
 const getHomeFeedPosts = async (req, res) => {
-	const visitorID = parseInt(res.locals.userID);
+	const userID = parseInt(res.locals.userID);
+
+	const { page, size, betweenFront, betweenBack } =
+		calculatePaginationIndexes(req);
 
 	const homeFeedPostsArray = [];
 
 	try {
-		const { rows } = await pool.queryToDatabase(
+		const homeFeedPostsArrayData = await pool.queryToDatabase(
 			`
-            SELECT 
-            leader_id AS owner_id,
-            posts.id AS post_id,
-            ROW_NUMBER() OVER (ORDER BY posts.created_at DESC) AS index
-            FROM following 
-            JOIN posts
-            ON posts.user_id=leader_id
-            WHERE follower_id=$1;
+			SELECT
+			id AS post_id,
+			created_at, 
+			post_views,
+			user_id AS owner_id
+			FROM (
+				SELECT
+				*,
+				ROW_NUMBER() OVER (ORDER BY created_at DESC) AS index
+				FROM posts
+				JOIN (
+					SELECT 
+					leader_id,
+					follower_id
+					FROM following
+					WHERE follower_id=$1
+				) AS f
+				ON posts.user_id=f.leader_id
+			) AS p
+			WHERE index BETWEEN $2 AND $3;
             `,
-			[visitorID]
+			[userID, betweenFront, betweenBack]
 		);
 
-		for (let leaderPost of rows) {
-			const ownerID = parseInt(leaderPost.owner_id);
-			const postID = parseInt(leaderPost.post_id);
+		const nextHomeFeedPostData = await pool.queryToDatabase(
+			`
+			SELECT
+			id AS post_id,
+			created_at, 
+			post_views,
+			user_id AS owner_id
+			FROM (
+				SELECT
+				*,
+				ROW_NUMBER() OVER (ORDER BY created_at DESC) AS index
+				FROM posts
+				JOIN (
+					SELECT 
+					leader_id,
+					follower_id
+					FROM following
+					WHERE follower_id=$1
+				) AS f
+				ON posts.user_id=f.leader_id
+			) AS p
+            WHERE index > $2
+            LIMIT 1;
+			`,
+			[userID, betweenBack]
+		);
 
-			const postImagesData = await PostRepo.getPostImages(postID);
+		const nextAPIEndpoint = nextHomeFeedPostData.rows[0]
+			? `/post/feed?page=${page + 1}&size=${size}`
+			: null;
 
-			const postOwnerData = await PostRepo.getPostOwner(ownerID);
+		for (let homeFeedPost of homeFeedPostsArrayData.rows) {
+			const ownerID = parseInt(homeFeedPost.owner_id);
+			const postID = parseInt(homeFeedPost.post_id);
 
-			const postCaptionsData = await PostRepo.getPostCaptions(postID);
+			const homeFeedPostImageData = await PostRepo.getPostImages(postID);
 
-			const postTotalLikesData = await PostRepo.getPostTotalLikes(postID);
+			const homeFeedPostOwnerData = await UserRepo.getUserByID(ownerID);
 
-			const postTotalCommentsData = await PostRepo.getPostTotalComments(postID);
+			const homeFeedPostCaptionsData = await PostRepo.getPostCaptions(postID);
 
-			const postIsLikedData = await PostRepo.getPostIsLiked(visitorID, postID);
+			const homeFeedPostTotalLikesData = await PostRepo.getPostTotalLikes(
+				postID
+			);
 
-			const postIsBookmarkedData = await PostRepo.getPostIsBookmarked(
-				visitorID,
+			const homeFeedPostTotalCommentsData = await PostRepo.getPostTotalComments(
+				postID
+			);
+
+			const homeFeedPostIsLikedData = await PostRepo.getPostIsLiked(
+				userID,
+				postID
+			);
+
+			const homeFeedPostIsBookmarkedData = await PostRepo.getPostIsBookmarked(
+				userID,
 				postID
 			);
 
 			homeFeedPostsArray.push({
 				post_id: postID,
-				post_owner: postOwnerData,
-				post_images: postImagesData,
-				post_captions: postCaptionsData,
-				post_total_likes: postTotalLikesData,
-				post_total_comments: postTotalCommentsData,
-				post_is_liked: postIsLikedData,
-				post_is_bookmarked: postIsBookmarkedData,
+				post_owner: homeFeedPostOwnerData,
+				post_images: homeFeedPostImageData,
+				post_captions: homeFeedPostCaptionsData,
+				post_total_likes: homeFeedPostTotalLikesData,
+				post_total_comments: homeFeedPostTotalCommentsData,
+				post_is_liked: homeFeedPostIsLikedData,
+				post_is_bookmarked: homeFeedPostIsBookmarkedData,
 			});
 		}
 
 		res.send({
-			home_feed_posts: homeFeedPostsArray,
+			contents: homeFeedPostsArray,
+			next: nextAPIEndpoint,
 		});
 	} catch (error) {
+		console.log(error);
 		res.send({
 			error: {
 				catch: "There has been an error while fetching data for your home feed",
