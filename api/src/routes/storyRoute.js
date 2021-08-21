cf5562ac5202b23299d2076587f09e18011c4e06
Joohyung Lee <something1712@gmail.com>
2021-08-21 09:06:14 -0500

    route to fetch story IDs of individual user

--- api/src/routes/storyRoute.js
+++ api/src/routes/storyRoute.js
@@ -8,6 +8,31 @@ const authenticateToken = require("../middlewares/user/authenticateToken");
 
 const storyRouter = express.Router();
 
+storyRouter.get("/story/ids/:userID", async (req, res) => {
+	const userID = parseInt(req.params.userID);
+
+	try {
+		const storyIDsArrayData = await pool.queryToDatabase(
+			`
+		SELECT
+		id
+		FROM stories
+		WHERE user_id=$1
+		`,
+			[userID]
+		);
+
+		res.send(storyIDsArrayData.rows.map((idObject) => idObject.id));
+	} catch (error) {
+		res,
+			send({
+				error: {
+					catch: "There has been an error while fetching user's story ids",
+				},
+			});
+	}
+});
+
 storyRouter.get("/story/backgrounds", async (req, res) => {
 	try {
 		const { rows } = await pool.queryToDatabase(
